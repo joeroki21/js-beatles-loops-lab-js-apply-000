86964c4aa3111d09accb585e134100f0e114c840
Joseph Lawson <josephdlawson21@gmail.com>
2017-10-24 01:28:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function theBeatlesPlay(musicians, instruments){
+  debugger;
+  var result = []
+  for (var i = 0; i < musicians.length; i++){
+    result.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return result
+}
